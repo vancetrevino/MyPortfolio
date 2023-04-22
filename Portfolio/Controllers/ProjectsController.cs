2180ae1439caa1db638c423dca968e43fcec23b1
Vance Trevino <trevino.vance@gmail.com>
2023-04-21 22:27:03 -0500

    Add project files.

---
+++ Portfolio/Controllers/ProjectsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly ILogger<ProjectsController> _logger;
+
+        public ProjectsController (ILogger<ProjectsController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: ProjectsController
+        public ActionResult Projects()
+        {
+            return View();
+        }
+    }
+}
